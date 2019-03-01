acf9efec191491085cce706332daa954461c46be
Lucas ALLOIN <lucas.alloin@epitech.eu>
2019-03-02 00:10:31 +0100

    --unique

--- sources/Main.hs
+++ sources/Main.hs
@@ -49,9 +49,15 @@ launchMode mod n alphabet
 unique::String -> String -> IO()
 unique n alphabet = do
         let convert_n = read n::Int
-        putStrLn "unique"
-        putStrLn n
-        putStrLn alphabet
+        input <- getLine
+        input2 <- getLine
+        let check_input = removeMemberOfValueFromString 0 input alphabet
+        let check_input2 = removeMemberOfValueFromString 0 input2 alphabet
+        if length input == length (alphabet) ^ convert_n && length input2 == length (alphabet) ^ convert_n && deBruijnCheck input convert_n && deBruijnCheck input2 convert_n && check_input == "" && check_input2 == "" && not (input == input2)
+                then
+                    putStrLn "OK"
+                else
+                    putStrLn "KO"
 
 check::String -> String -> IO()
 check n alphabet = do
